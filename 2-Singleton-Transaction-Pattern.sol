7bf4f29f5711b9aee36d1dba4a5182b959f25620
Tadej Vengust <vengust.tadej@gmail.com>
2019-02-06 22:38:16 +0100

    Update example contract 2.

--- 2-Singleton-Transaction-Pattern.sol
+++ 2-Singleton-Transaction-Pattern.sol
@@ -1,21 +1,34 @@
-pragma solidity ^0.4.23;
-import "@0xcert/ethereum-erc721/contracts/tokens/NFToken.sol";
+pragma solidity 0.5.2;
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/erc721-enumerable.sol";
 
 contract SuSquaresTests
 {
-    /// @notice Deploy test contract with subject to be used for all tests
-    constructor(ERC721 testSubject)
-    {
-        testIsTotalSupply10000(testSubject);
-    }
 
-    /// @notice Test token supply invariant
-    function testIsTotalSupply10000(ERC721 testSubject) returns (bool)
-    {
-    	bool testResult;
-    	// Perform complicated testing.
-    	// Assume this test implementation must make state changes to mainnet to work,
-        // for some good reason.
-    	return testResult;
-    }
+  /**
+   * @notice Deploy test contract with subject to be used for all tests.
+   */
+  constructor(
+    ERC721Enumerable _testSubject
+  )
+    public
+  {
+    testIsTotalSupply10000(_testSubject);
+  }
+
+  /**
+   * @notice Test token supply invariant.
+   */
+  function testIsTotalSupply10000(
+    ERC721Enumerable _testSubject
+  )
+    public
+    view
+    returns (bool testResult)
+  { 
+    // Perform complicated testing.
+    // Assume this test implementation must make state changes to mainnet to work,
+    // for some good reason.
+    testResult = _testSubject.totalSupply() == 10000;
+  }
+  
 }
